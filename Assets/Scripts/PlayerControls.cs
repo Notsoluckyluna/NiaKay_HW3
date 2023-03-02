c56d05779b7416b1ea90f24a69c9088fb26141e5
Notsoluckyluna <116504646+Notsoluckyluna@users.noreply.github.com>
2023-03-01 22:30:20 -0600

    tried to adjust user controls; failed lol

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,6 +8,14 @@ public class PlayerControls : MonoBehaviour
     float movementSpeed = 6f;
     float jumpForce = 5f;
 
+    
+
+    
+
+  
+  
+
+
     private void FixedUpdate()
     {
 
@@ -28,7 +36,22 @@ public class PlayerControls : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput);
+       
 
+        //if (direction.magnitude >= 0.1f)
+        {
+           // float targetAngle = Mathf.Atan2(direction.x, direction.z)* Mathf.Rad2Deg;
+            //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+           // transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+           //controller.Move(direction * speed * Time.deltaTime);
+           // Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f)* Vector3.forward;
+
+
+        }
+
+      
 
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
